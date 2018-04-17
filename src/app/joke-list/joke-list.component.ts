2d36f240dab756b9c809e75467b4e820bd8d45f1
Harjeet <singh.fulda@gmail.com>
2018-04-17 22:27:26 +0200

    joke-app: hide and show joke answers
    Joke list with toggle to hide and show answer

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -13,20 +13,27 @@ export class JokeListComponent implements OnInit {
     this.jokes = [
                     {
                     setup: "What did the cheese say when it looked in the mirror?",
-                    punchline: "Hello-Me (Halloumi)"
+                    punchline: "Hello-Me (Halloumi)",
+                    hide:"true"
                     },
                     {
                     setup: "What kind of cheese do you use to disguise a small horse?",
-                    punchline: "Mask-a-pony (Mascarpone)"
+                    punchline: "Mask-a-pony (Mascarpone)",
+                    hide:"true"
                     },
                     {
                     setup: "A kid threw a lump of cheddar at me",
-                    punchline: "I thought ‘That’s not very mature’"
+                    punchline: "I thought ‘That’s not very mature’",
+                    hide:"true"
                     },
                 ];
    }
 
   ngOnInit() {
   }
+
+toggle(joke) {
+joke.hide = !joke.hide;
+}
 
 }
